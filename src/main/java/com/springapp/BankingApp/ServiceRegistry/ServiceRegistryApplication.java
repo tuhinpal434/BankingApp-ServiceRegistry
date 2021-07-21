994015917ce44b4130b49b36870166e0c2760f24
Tuhin Pal <tuhinpal.434@gmail.com>
2021-07-21 18:47:08 +0530

    Adding SpringBootServletInitializer

--- src/main/java/com/springapp/BankingApp/ServiceRegistry/ServiceRegistryApplication.java
+++ src/main/java/com/springapp/BankingApp/ServiceRegistry/ServiceRegistryApplication.java
@@ -2,11 +2,18 @@ package com.springapp.BankingApp.ServiceRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceRegistryApplication {
+public class ServiceRegistryApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ServiceRegistryApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
